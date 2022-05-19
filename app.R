9e9f29d6981b090702738e774be6edf9a84d148b
Jaime <jllera@lab04.bioinfo.cipf.es>
2022-05-19 15:12:38 +0200

    Added some buttons and played with observeEvent and reactiveValues

--- app.R
+++ app.R
@@ -6,26 +6,30 @@ library(shiny)
 ui <- fluidPage(
   sliderInput(inputId= "num", label = "Choose a number", min=10, max=100, value=10),
   textInput(inputId = "title", label = "Write a title", value = "Random Normal Values"),
+  
+  actionButton(inputId = "norm", label = "Normal"),
+  actionButton(inputId = "unif", label = "Uniform"),
+  
   plotOutput("hist"),
   verbatimTextOutput("stats")
   
 ) 
 
 server <- function(input, output) {
+
   
-  data <- reactive({
-    rnorm(input$num)
-  })
+  rv <- reactiveValues(data = rnorm(100))
   
-  
+  observeEvent(input$norm, {rv$data <- rnorm(input$num) })
+  observeEvent(input$unif, {rv$data <- runif(input$num) })
   
   output$hist <- renderPlot({ 
     title <- "100 random normal values"
-    hist(data(), main = isolate({input$title}))
+    hist(rv$data, main = isolate({input$title}))
     })
   
   output$stats <- renderPrint({
-    summary(data())
+    summary(rv$data)
   })
   
   }
