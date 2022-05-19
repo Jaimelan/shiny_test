f85f484107dff750165f7f66e64efc068df82ca7
Jaime <jllera@lab04.bioinfo.cipf.es>
2022-05-19 15:43:12 +0200

    Added some style options

--- app.R
+++ app.R
@@ -4,12 +4,20 @@ library(shiny)
 
 # 2 inputs
 ui <- fluidPage(
+  # Html and style functions
+  tags$h1("Test for a Shiny app"),
+  tags$a(href = "https://github.com/Jaimelan", "My GitHub"),
+  
+  tags$hr(),
+  # Inputs
   sliderInput(inputId= "num", label = "Choose a number", min=10, max=100, value=10),
   textInput(inputId = "title", label = "Write a title", value = "Random Normal Values"),
   
   actionButton(inputId = "norm", label = "Normal"),
   actionButton(inputId = "unif", label = "Uniform"),
+    
   
+  # Reactive outputs
   plotOutput("hist"),
   verbatimTextOutput("stats")
   
