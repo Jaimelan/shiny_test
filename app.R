535051c97567ad05da693efead3e87c62f9b79eb
Jaime <jllera@lab04.bioinfo.cipf.es>
2022-05-19 14:38:34 +0200

    Added some funtions like reactive objects, isolate() functions and modules

--- app.R
+++ app.R
@@ -1,19 +1,34 @@
 library(shiny)
 
 # Se añade en fluidpage el contenido
+
+# 2 inputs
 ui <- fluidPage(
   sliderInput(inputId= "num", label = "Choose a number", min=10, max=100, value=10),
-  plotOutput("hist")
+  textInput(inputId = "title", label = "Write a title", value = "Random Normal Values"),
+  plotOutput("hist"),
+  verbatimTextOutput("stats")
   
-                
 ) 
 
 server <- function(input, output) {
+  
+  data <- reactive({
+    rnorm(input$num)
+  })
+  
+  
+  
   output$hist <- renderPlot({ 
     title <- "100 random normal values"
-    hist(rnorm(input$num), main = title)
+    hist(data(), main = isolate({input$title}))
     })
+  
+  output$stats <- renderPrint({
+    summary(data())
+  })
   
   }
 
 shinyApp(ui = ui, server = server)
+
