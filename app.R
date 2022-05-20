06a8ab7659c5292f75ed52c9d069726964fef52e
Jaime <jllera@lab04.bioinfo.cipf.es>
2022-05-20 11:29:44 +0200

    added some css styling from a public css style file 'Bootswatch: lumen' to try out some styling

--- app.R
+++ app.R
@@ -1,29 +1,44 @@
+
+### Title: Example of an app built with shiny
+### Auth: Jaime LLera
+### Date: 20/05/22
+
 library(shiny)
 
+
+
+## runApp() to load images from www
 # Se añade en fluidpage el contenido
 
 # 2 inputs
 ui <- fluidPage(
+  includeCSS("www/bootstrap.css"),
+  
   # Html and style functions
-  h1("Test for a Shiny app"),
+  h1("Test for a Shiny app", class="h1"),
   a(href = "https://github.com/Jaimelan", "My GitHub"),
   
-  hr(),
+  hr(),  # Horizontal rule
   
- 
-
   fluidRow(
+    # column(4, tags$img(width=250, src = "mage.png")), # embed an image
   # Inputs
-    column(6, sliderInput(inputId= "num", label = "Choose a number", min=10, max=100, value=10)),
-    column(6, textInput(inputId = "title", label = "Write a title", value = "Random Normal Values")),
+    column(4, sliderInput(inputId= "num", 
+                          label = "Choose a number", 
+                          min=10, max=100, value=10)), # Add a slider input object
+  
+    column(4, textInput(inputId = "title", label = "Write a title", value = "Random Normal Values")), # Add text input
   ),
+  # Encapsulate objects inside a panel
   wellPanel(
-    actionButton(inputId = "norm", label = "Normal"),
-    actionButton(inputId = "unif", label = "Uniform"),
+    # Action button to switch between two types of output
+    actionButton(inputId = "norm", label = "Normal", class="btn-danger"), 
+    actionButton(inputId = "unif", label = "Uniform", class="btn-warning"),
     ),
   
   # Reactive outputs
   tabsetPanel(
+    # Outputs separated into tabs 
     tabPanel("Histogram", plotOutput("hist")),
     tabPanel("Stat summary", verbatimTextOutput("stats"))
   )
@@ -31,12 +46,13 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-  
+  # Reactive values store reactive dependencies that change whenevr a reaction is triggered
   rv <- reactiveValues(data = rnorm(100))
-  
+  # When "button" is pressed the rv$data vals will be changed to the distribution
   observeEvent(input$norm, {rv$data <- rnorm(input$num) })
   observeEvent(input$unif, {rv$data <- runif(input$num) })
   
+  # Rules for the histogra output
   output$hist <- renderPlot({ 
     title <- "100 random normal values"
     hist(rv$data, main = isolate({input$title}))
@@ -50,3 +66,4 @@ server <- function(input, output) {
 
 shinyApp(ui = ui, server = server)
 
+runApp()
